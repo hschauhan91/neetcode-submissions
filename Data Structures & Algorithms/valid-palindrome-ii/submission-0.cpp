32c2b21d4123c644f89369aa34de436f198ab92c
Harsh Singh Chauhan <hsking.hh@gmail.com>
2026-07-24 23:03:49 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    bool isPalindrome(int i,int j,string s){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else return (isPalindrome(i+1,j,s) || isPalindrome(i,j-1,s));
+        }
+
+        return true;
+    }
+};
